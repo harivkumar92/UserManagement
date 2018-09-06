b060a142aa35c5928352ecda4ce1d2742fef3fe0
Hari Kumar <harivkumar91@gmail.com>
2018-09-06 15:07:24 +0530

    Changed Model class

--- Tryout/modelBaseclass.js
+++ Tryout/modelBaseclass.js
@@ -13,18 +13,9 @@ const sequelize = new Sequelize('usermanagement', 'user', 'password', {
 });
 
 class ModelBaseClass{
-
-    setUser(){
-        const User = sequelize.define('user', {
-            user_email: Sequelize.STRING(100),
-            user_phone: Sequelize.INTEGER,
-            user_password: { type: Sequelize.CHAR(60).BINARY, allowNull: false },
-            user_activation_code: Sequelize.STRING(100),
-            user_status: { type: Sequelize.STRING(10), allowNull: false, defaultValue: 'inactive' },
-            created_At: Sequelize.DATE,
-            updated_At: Sequelize.DATE
-        });
-        return User;
+    constructor(){
+        this.created_At;
+        this.updated_At;
     }
 }
 
--- Tryout/modeluser.js
+++ Tryout/modeluser.js
@@ -13,7 +13,39 @@ const sequelize = new Sequelize('usermanagement', 'user', 'password', {
     }
 });
 
-const user = new BaseClass();
+class User extends BaseClass{
+    constructor(){
+        this.user_email;
+        this.user_phone;
+        this.user_password;
+        this.user_activation_code;
+        this.user_status;
+    }
+
+    createUser(){
+        const User = sequelize.define('user', {
+            user_email: Sequelize.STRING(100),
+            user_phone: Sequelize.INTEGER,
+            user_password: { type: Sequelize.CHAR(60).BINARY, allowNull: false },
+            user_activation_code: Sequelize.STRING(100),
+            user_status: { type: Sequelize.STRING(10), allowNull: false, defaultValue: 'inactive' },
+            created_At: Sequelize.DATE,
+            updated_At: Sequelize.DATE
+        })
+        return User;
+    }
+}
+
+/*
+const User = sequelize.define('user', {
+    user_email: Sequelize.STRING(100),
+    user_phone: Sequelize.INTEGER,
+    user_password: { type: Sequelize.CHAR(60).BINARY, allowNull: false },
+    user_activation_code: Sequelize.STRING(100),
+    user_status: { type: Sequelize.STRING(10), allowNull: false, defaultValue: 'inactive' },
+    created_At: Sequelize.DATE,
+    updated_At: Sequelize.DATE
+
 
 User = user.setUser();
 
@@ -24,3 +56,4 @@ sequelize
    // sequelize.close()
 //);
 module.exports = User;
+*/
