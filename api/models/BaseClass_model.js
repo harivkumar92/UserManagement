b57658d4249c24bc604e3e8a19b3d62bc214616a
Hari Kumar <harivkumar91@gmail.com>
2018-09-08 16:03:58 +0530

    Added a base class for the models

---
+++ api/models/BaseClass_model.js
@@ -0,0 +1,9 @@
+//this base class initializes fields common to all the models
+class BaseClass{
+    constructor(){
+        this.created_At;
+        this.updated_At;
+    }
+}
+
+module.exports = BaseClass;
